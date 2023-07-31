6996c905def0391609864deb4c251f6cfe1253ee
Sara Spena <saraspena98@gmail.com>
2023-07-31 17:02:41 +0200

    push

--- src/components/table/PatientsTable.tsx
+++ src/components/table/PatientsTable.tsx
@@ -1,6 +1,11 @@
-import { Dipendente } from "../../api";
+import { useNavigate } from "react-router-dom";
+import { Dipendente, DipendenteDTO } from "../../api";
 import ClientSideTable from "./ClientSideTable";
-import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import {
+  GridColDef,
+  GridRowParams,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
 
 const columns: GridColDef<Dipendente>[] = [
   { field: "id", headerName: "ID" },
@@ -38,8 +43,17 @@ type Props = {
 };
 
 function PatientsTable({ patients }: Props) {
+  const navigate = useNavigate();
+
   return (
-    <ClientSideTable header="Pazienti" rows={patients} columns={columns} />
+    <ClientSideTable
+      header="Pazienti"
+      rows={patients}
+      columns={columns}
+      onRowClick={(item: GridRowParams<DipendenteDTO>) =>
+        navigate(`/contacts/${item.row.id}`)
+      }
+    />
   );
 }
 
--- src/pages/medical-days/MedicalDaysPage.tsx
+++ src/pages/medical-days/MedicalDaysPage.tsx
@@ -1,7 +1,160 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Checkbox,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Modal,
+  Select,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { useState } from "react";
 import MedicalDayTable from "../../components/table/MedicalDayTable";
+import { useQueries } from "@tanstack/react-query";
+import {
+  DipendenteControllerApi,
+  MedicoControllerApi,
+  RangeFilterLocalDate,
+  SedeControllerApi,
+} from "../../api";
 
 function MedicalDaysPage() {
-  return <MedicalDayTable />;
+  const months = () => {
+    const newDate = new Date();
+    const currentMonth = newDate.getMonth();
+
+    newDate.setMonth(currentMonth + 1);
+
+    return newDate;
+  };
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  const [provincia, setProvincia] = useState("");
+  const [sedeId, setSedeId] = useState("");
+  const [medico, setMedico] = useState("");
+  const [{ data: sedi }, { data: medici }, { data: dipendenti }] = useQueries({
+    queries: [
+      {
+        queryKey: ["medicalday-sedi"],
+        queryFn: () => new SedeControllerApi().findAll1(),
+      },
+      {
+        queryKey: ["medicalday-medici"],
+        queryFn: () =>
+          new MedicoControllerApi().findAll2(
+            { sedeId: { equals: sedeId as unknown as number } },
+            {},
+          ),
+        enabled: sedeId !== undefined,
+      },
+      {
+        queryKey: ["medicalday-dipendenti-scadenza"],
+        queryFn: () =>
+          new DipendenteControllerApi().findAll5(
+            {
+              scadenzaVisitaDal: {
+                greaterOrEqualThan:
+                  new Date() as unknown as RangeFilterLocalDate,
+              },
+              scadenzaVisitaAl: {
+                lessOrEqualThan: months as unknown as RangeFilterLocalDate,
+              },
+            },
+            { size: 6 },
+          ),
+      },
+    ],
+  });
+
+  console.log(medici + "medici e sedeid: " + sedeId);
+  return (
+    <>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <Card style={{ width: "80%" }}>
+          <CardHeader
+            component={() => (
+              <Box style={{ backgroundColor: theme.palette.primary.light }}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Nuovo Medical Day
+                </Typography>
+              </Box>
+            )}
+          ></CardHeader>
+          <CardContent>
+            <InputLabel id="demo-simple-select-label">Provincia</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={provincia}
+              label="Provincia"
+              onChange={(e) => {
+                setProvincia(e.target.value);
+                setSedeId(e.target.value);
+              }}
+            >
+              {sedi?.data
+                ?.map((s) => (
+                  <MenuItem key={s.id} value={s.id}>
+                    {s.denominazione}
+                  </MenuItem>
+                ))
+                .sort((a, b) =>
+                  a.props.children.localeCompare(b.props.children),
+                )}
+            </Select>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={medico}
+              label="Medico"
+              onChange={(e) => {
+                setMedico(e.target.value);
+              }}
+            >
+              {medici?.data.content
+                ?.map((m) => (
+                  <MenuItem key={m.id} value={m.nome}>
+                    {m.nome + " " + m.cognome}
+                  </MenuItem>
+                ))
+                .sort((a, b) =>
+                  a.props.children.localeCompare(b.props.children),
+                )}
+            </Select>
+            {dipendenti?.data.content?.map((d) => (
+              <FormControlLabel
+                key={d.id}
+                label={d.nome + " " + d.cognome + " " + d.scadenzaVisita}
+                control={<Checkbox />}
+              />
+            ))}
+          </CardContent>
+        </Card>
+      </Modal>
+      <MedicalDayTable />
+    </>
+  );
 }
 
 export default MedicalDaysPage;
